a57dd5535f4fb1a0ca431b4e18a43538116239be
Jannes <jannesventer09@gmail.com>
2020-07-07 23:29:37 +0200

    Added temporary email test

--- KontactKeepers/KontactKeepers/Program.cs
+++ KontactKeepers/KontactKeepers/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,23 @@ namespace KontactKeepers
     {
         static void Main(string[] args)
         {
+            var messageToSend = new MimeMessage
+            {
+                Body = new TextPart("plain") { Text = "Hello world!" },
+                Subject = "Test",
+            };
+            messageToSend.From.Add(new MailboxAddress("Jannes Venter", "jannesventer09@gmail.com"));
+            messageToSend.To.Add(new MailboxAddress("Devilliers du Plessis", "jannesventer09@gmail.com"));
+            using (var client = new SmtpClient())
+            {
+                client.Connect("host21.axxesslocal.co.za", 465, true);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("KontactKeeper@campus-kiosk.ac.za", "g4Ha2QtlMi7D");
+
+                client.Send(messageToSend);
+                client.Disconnect(true);
+            }
         }
     }
 }
