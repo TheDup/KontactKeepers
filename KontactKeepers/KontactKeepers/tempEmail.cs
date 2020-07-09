77db771f3b733f7fea76ffc438f47bb7be1a2827
Jannes <jannesventer09@gmail.com>
2020-07-09 14:25:06 +0200

    modified email class

--- KontactKeepers/KontactKeepers/tempEmail.cs
+++ KontactKeepers/KontactKeepers/tempEmail.cs
@@ -8,17 +8,21 @@ namespace KontactKeepers
 {
     class tempEmail
     {
-		private string emailaddress;
+        private string emailAdressUName = "KontactKeeper@campus-kiosk.ac.za";
+        private string mailPass = "g4Ha2QtlMi7D";
+        private string incomingSSLServer = "host21.axxesslocal.co.za";
+        private string IMAPPort = "993";
+        private string POP3Port = "995";
+        private string OutgoingServer = "host21.axxesslocal.co.za";
+        private string SMTPPort = "465";
 
-		public string EmailAddress
-		{
-			get { return emailaddress; }
-			set { emailaddress = value; }
-		}
 
-		public tempEmail(string emailaddress)
-		{
-			this.emailaddress = emailaddress;
-		}
-	}
+        // Non SSL settings
+        //Username: Your Email Address
+        //Incoming Server: Your Domain
+        //IMAP Port: 143
+        //POP3 Port: 110
+        //Outgoing Server: Your Domain
+        //SMTP Port: 25
+    }
 }
