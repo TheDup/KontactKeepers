7fc0add158f196ec8ac2b6159a0325da52b89fd7
Jannes <jannesventer09@gmail.com>
2020-08-11 18:13:46 +0200

    fix attempt 1

--- KontactKeepers/FaceBookAPI/FacebookAPI.cs
+++ KontactKeepers/FaceBookAPI/FacebookAPI.cs
@@ -8,16 +8,17 @@ namespace FmAPI
 {
     class FBAccess
     {
+        private string messengerAPItoken = "EAAiZApOi1Kb4BAGlGVA93ccXecvz74t9iG3o98VfbJBC1MkbZAX8k3a41LKAjTKdZATP0e7SQZCmY7sAsT8sbB2wctMob41bJwPlq2TW4I0E53BzE1CE9eeCcg0wrGhlCuJlr6HPkNJ0h40G36GKZBH0gv5oD7mNWWzesshhd8QZDZD";
         static void Main(string[] args)
         {
-            MessengerAPI obj = new MessengerAPI("EAAiZApOi1Kb4BAGlGVA93ccXecvz74t9iG3o98VfbJBC1MkbZAX8k3a41LKAjTKdZATP0e7SQZCmY7sAsT8sbB2wctMob41bJwPlq2TW4I0E53BzE1CE9eeCcg0wrGhlCuJlr6HPkNJ0h40G36GKZBH0gv5oD7mNWWzesshhd8QZDZD");
+            MessengerAPI obj = new MessengerAPI(messengerAPItoken);
             obj.SendTextMessageAsync(660746514005471, "Hello");
         }
 
 
         static void SendMessage()
         {
-            string facebookToken = "your facebook token here";
+            string facebookToken = messengerAPItoken;
             var client = new FacebookClient(facebookToken);
 
             dynamic result = client.Get("me/inbox", null);
