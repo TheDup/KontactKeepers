887e676d52bce8b215d4b46254eb84e8eff2c93f
Jannes <jannesventer09@gmail.com>
2020-08-16 20:19:37 +0200

    Added WhatsappAPI

---
+++ KontactKeepers/WhatsappAPI/TwilioHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppSelenium
+{
+    class TwilioHelper
+    {
+        public List<User> insertUsers()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User());
+        }
+       
+    }
+}
