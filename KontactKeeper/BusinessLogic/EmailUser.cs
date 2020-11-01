653355cdfd976fa4a87945f622c442a47940c27e
Jannes <jannesventer09@gmail.com>
2020-11-01 11:50:00 +0200

    Added user classes EmailUser and WAUser

---
+++ KontactKeeper/BusinessLogic/EmailUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace BusinessLogic
+{
+    class EmailUser
+    {
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private DateTime lastseen;
+
+        public DateTime LastSeen
+        {
+            get { return lastseen; }
+            set { lastseen = value; }
+        }
+
+        public EmailUser(string email, DateTime lastseen)
+        {
+            this.email = email;
+            this.lastseen = lastseen;
+        }
+    }
+}
---
+++ KontactKeeper/BusinessLogic/WAUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class WAUser
+    {
+        private string cellnumber;
+
+        public string CellNumber
+        {
+            get { return cellnumber; }
+            set { cellnumber = value; }
+        }
+        private SqlDateTime lastseen;
+
+        public SqlDateTime LastSeen
+        {
+            get { return lastseen; }
+            set { lastseen = value; }
+        }
+
+        public WAUser(string cellnumber, SqlDateTime lastseen)
+        {
+            this.cellnumber = cellnumber;
+            this.lastseen = lastseen;
+        }
+    }
+}
