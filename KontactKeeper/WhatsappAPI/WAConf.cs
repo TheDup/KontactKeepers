f879d45666a2f69fd13a3d5151c601da9537ed36
Jannes <jannesventer09@gmail.com>
2020-11-05 02:36:40 +0200

    Added Twilio server

---
+++ KontactKeeper/WhatsappAPI/WAConf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace WhatsappAPI
+{
+    public class WAConf
+    {
+        public void SendMessage()
+        {
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                from: new Twilio.Types.PhoneNumber("+14155238886"),
+                to: new Twilio.Types.PhoneNumber("+27721752568")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
